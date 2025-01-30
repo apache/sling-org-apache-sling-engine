12f8ac468f262717f660cd90f2a6f27fa1fdd4ac
Carsten Ziegeler <cziegeler@apache.org>
2025-01-30 17:24:51 +0100

    SLING-12640 : Setting Sling filter service property sling.filter.scope to an invalid value does not disable the filter.

--- src/main/java/org/apache/sling/engine/impl/filter/ServletFilterManager.java
+++ src/main/java/org/apache/sling/engine/impl/filter/ServletFilterManager.java
@@ -313,7 +313,6 @@ public class ServletFilterManager {
         boolean used = false;
         for (String scope : scopes) {
             scope = scope.toUpperCase();
-            used = true;
             try {
                 FilterChainType type = FilterChainType.valueOf(scope.toString());
                 getFilterChain(type).addFilter(filter, predicate, serviceId, order, orderSource, mbean);
@@ -324,6 +323,7 @@ public class ServletFilterManager {
                     getFilterChain(FilterChainType.FORWARD)
                             .addFilter(filter, predicate, serviceId, order, orderSource, mbean);
                 }
+                used = true;
             } catch (final IllegalArgumentException iae) {
                 log.warn("Filter service {} has invalid value {} for scope. Value is ignored", reference, scope);
             }
@@ -333,7 +333,6 @@ public class ServletFilterManager {
                     "Filter service {} has been registered without a valid {} property. Using default value.",
                     serviceId,
                     EngineConstants.SLING_FILTER_SCOPE);
-            getFilterChain(FilterChainType.REQUEST).addFilter(filter, predicate, serviceId, order, orderSource, mbean);
         }
     }
 
