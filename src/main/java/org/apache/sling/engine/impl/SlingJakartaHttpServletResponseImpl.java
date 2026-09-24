0c0ab4c002dbb53b0547594336fca65f8a4821da
Joerg Hoh <joerghoh@apache.org>
2026-09-24 11:29:18 +0200

    spotless

--- src/main/java/org/apache/sling/engine/impl/SlingJakartaHttpServletResponseImpl.java
+++ src/main/java/org/apache/sling/engine/impl/SlingJakartaHttpServletResponseImpl.java
@@ -30,9 +30,8 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Objects;
 import java.util.Map;
-
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Supplier;
 import java.util.regex.Matcher;
